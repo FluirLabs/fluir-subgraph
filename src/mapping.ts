75d82342aef6220ba0dd947b7c871eecbf03feed
Scifidreams <likemneulett@gmail.com>
2024-11-20 07:06:59 +0000

    update token mapping

--- src/mapping.ts
+++ src/mapping.ts
@@ -65,18 +65,66 @@ export  function  handleCreateStream(ev: CreateStreamEvent): void {
 
 stream.remainingBalance = streamDeposit;
 stream.createAt = ev.block.timestamp;
+stream.remainingBalance = stream.remainingBalance.minus(streamCliffAmount);
+stream.lastWithdrawTime = ev.params.startTime;
+stream.closed = false;
+stream.isPaused = false;
+
+
+//to be calculated for
+stream.withdrawnBalance = BigDecimal.fromString("0");
 stream.pauseAt = BigInt.fromI32(0);
+stream.pauseBy = Address.zero();
+stream.accPauseTime = BigInt.fromI32(0);
+stream.feeBalance =BigDecimal.fromString("0");
+
+
+//cliff vesting logic 
+if(ev.block.timestamp >= ev.params.cliffTime || ev.params.cliffAmount == BigInt.fromI32(0)){
+  stream.cliffDone = true;
+  let fee = streamCliffAmount.times(token.feeRate)
+} else{
+  stream.cliffDone = false;
+}
 
 
 
-stream.save;
+
+
+
+stream.save();
 
 }
 
 
 
 export  function  handleTokenRegister(ev: TokenRegisterEvent):void{
-  let token = new RegisteredTokenLog(createEventID(ev));
+  //token Information 
+  let tokenName = fetchTokenName(ev.params.tokenAddress);
+  let tokenSymbol = fetchTokenSymbol(ev.params.tokenAddress);
+  let tokenDecimals = fetchTokenDecimals(ev.params.tokenAddress);
+
+  //load token entity
+  let token = TokenRegister.load(ev.params.tokenAddress.toHex());
+  if(token){
+    log.error("[handleTokenRegister] TokenRegister already exist {}", [ev.params.tokenAddress.toHex()]);
+    return;
+  }
+
+  token = new TokenRegister(ev.params.tokenAddress.toHex());
+  token.name = tokenName;
+  token.symbol = tokenSymbol;
+  token.decimals = tokenDecimals;
+  token.feeRate = convertTokenToDecimal(ev.params.feeRate, BigInt.fromI32(4));
+
+  token.withdrawnBalance = BigDecimal.fromString("0");
+  token.feeBalance = BigDecimal.fromString("0");
+  token.remainingBalance = BigDecimal.fromString("0");
+
+
+
+  token.save();
+
 }
 
 
